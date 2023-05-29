b490325cc1afa1715d52fe7f34db0d7c2076a3be
mustafacelebi15 <96596130+mustafacelebi15@users.noreply.github.com>
2023-05-29 23:41:47 +0300

    Takvim Uygulamasi
    takvime girilen zamanda bildirim yollaniyor

--- TakvimUygulaması/TakvimUygulaması/OlayTanımlamaForm.cs
+++ TakvimUygulaması/TakvimUygulaması/OlayTanımlamaForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
@@ -64,6 +65,7 @@ namespace TakvimUygulaması
             MessageBox.Show("Kaydedildi!");
             cmd.Dispose();
             connection.Close();
+            alarm();
         }
 
         private void buttongüncelle_Click(object sender, EventArgs e)
@@ -81,6 +83,7 @@ namespace TakvimUygulaması
             MessageBox.Show("Güncellendi!");
             cmd.Dispose();
             connection.Close();
+            alarm();
         }
 
         private void buttonsil_Click(object sender, EventArgs e)
@@ -98,5 +101,81 @@ namespace TakvimUygulaması
             TakvimForm frm = new TakvimForm();
             frm.ShowDialog();
         }
+        static void BildirimGonder(string baslik, string icerik)
+        {
+            NotifyIcon bildirim = new NotifyIcon();
+            bildirim.Icon = SystemIcons.Exclamation;
+            bildirim.Visible = true;
+            bildirim.BalloonTipTitle = baslik;
+            bildirim.BalloonTipText = icerik;
+            bildirim.ShowBalloonTip(5000);
+        }
+        private void alarm()
+        {
+            MySqlConnection connection = new MySqlConnection(connString);
+            connection.Open();
+            String sql = "SELECT *FROM planliste";
+            MySqlCommand cmd = connection.CreateCommand();
+            cmd.CommandText = sql;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<string> gunler = new List<string>();
+            List<string> saatler = new List<string>();
+            List<string> mesajlar = new List<string>();
+            List<string> aciklamalar = new List<string>();
+            while (reader.Read())
+            {
+                string data = reader.GetString(1);
+                string saat = reader.GetString(2);
+                string mesaj = reader.GetString(4);
+                string aciklama = reader.GetString(5);
+                gunler.Add(data);
+                saatler.Add(saat);
+                mesajlar.Add(mesaj);
+                aciklamalar.Add(aciklama);
+            }
+            string[] gunlers = gunler.ToArray();
+            string[] saatlers = saatler.ToArray();
+            string[] mesajlars = mesajlar.ToArray();
+            int boyut = saatlers.Length;
+            reader.Close();
+            connection.Close();
+            DateTime dateTime = DateTime.Now;
+            string datestr = dateTime.ToString("dd.MM.yyyy" + " 00:00:00");
+            int[,] saatdakika = new int[boyut, 2];
+            for (int i = 0; i < boyut; i++)
+            {
+                string[] timeParts = saatlers[i].Split(':');
+
+
+                saatdakika[i, 0] = Convert.ToInt32(timeParts[0]);
+
+                saatdakika[i, 1] = Convert.ToInt32(timeParts[1]);
+
+            }
+            for (int i = 0; i < gunlers.Length; i++)
+            {
+                if (datestr == gunlers[i])
+                {
+
+                    DateTime alarmZamani = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, saatdakika[i, 0], saatdakika[i, 1], 0);
+                    TimeSpan kalanSure = alarmZamani - dateTime;
+                    int kalanSureSaniye = (int)kalanSure.TotalSeconds;
+                    // Geri sayım başlat
+                    if (kalanSureSaniye > 0)
+                    {
+                        Thread.Sleep(kalanSureSaniye * 1000);
+                        BildirimGonder("Alarm: " + mesajlars[i], "AÇIKLAMA: " + aciklamalar[i]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bir sorun tespit edildi bir dakika beklemeniz rica ediliyor:");
+                        continue;
+                    }
+                }
+            }
+        }
+
+
     }
 }
+
--- TakvimUygulaması/TakvimUygulaması/TakvimForm.Designer.cs
+++ TakvimUygulaması/TakvimUygulaması/TakvimForm.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.buttonprevious = new System.Windows.Forms.Button();
             this.buttonnext = new System.Windows.Forms.Button();
@@ -41,8 +40,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.buttoncıkısyap = new System.Windows.Forms.Button();
             this.labeldate = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -164,24 +161,11 @@
             this.labeldate.Text = "MONTH YEAR";
             this.labeldate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(1201, 44);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(0, 16);
-            this.label8.TabIndex = 20;
-            // 
             // TakvimForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1296, 843);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.buttoncıkısyap);
             this.Controls.Add(this.labeldate);
             this.Controls.Add(this.label7);
@@ -218,7 +202,5 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button buttoncıkısyap;
         private System.Windows.Forms.Label labeldate;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label8;
     }
 }
